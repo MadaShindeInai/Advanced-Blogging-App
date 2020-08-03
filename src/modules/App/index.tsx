34a154c7a11772e3d801ecfad7633f2994a16dfd
Sergei Nepryakhin <sergey.nepryahin@dashboquet.com>
2020-08-03 21:32:57 +0300

    add eslint typescript prettier

---
+++ src/modules/App/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {StyleSheet, StatusBar, Text} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <Text style={styles.sectionTitle}>Hello world</Text>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionTitle: {
+    fontFamily: 'Kalam-Bold',
+    fontSize: 44,
+    color: Colors.black,
+    textAlign: 'center',
+    marginTop: 30,
+  },
+  sectionTitle2: {
+    fontFamily: 'Kalam-Light',
+    fontSize: 24,
+    color: Colors.black,
+  },
+  sectionTitle3: {
+    fontFamily: 'PermanentMarker-Regular',
+    fontSize: 24,
+    color: Colors.black,
+  },
+  sectionTitle4: {
+    fontFamily: 'RobotoMono-ThinItalic',
+    fontSize: 24,
+    color: Colors.black,
+  },
+  sectionDescription: {
+    fontFamily: 'RobotoMono-BoldItalic',
+    marginTop: 8,
+    fontSize: 18,
+    color: Colors.dark,
+  },
+});
+
+export default App;
