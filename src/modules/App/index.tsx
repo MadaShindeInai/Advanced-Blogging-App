c9e175b4b04ea0eb00032bafa46006fa897a1d29
Sergei Nepryakhin <sergey.nepryahin@dashboquet.com>
2020-08-04 18:19:30 +0300

    start implementing routing

--- src/modules/App/index.tsx
+++ src/modules/App/index.tsx
@@ -1,11 +1,24 @@
+import 'react-native-gesture-handler';
 import React, {FC} from 'react';
-import {Main} from '../Main';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {MainScreen} from '../../screens/MainScreen';
+import {AboutScreen} from '../../screens/AboutScreen';
+
+const Stack = createStackNavigator();
 
 const App: FC = () => {
   return (
-    <>
-      <Main />
-    </>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="MainScreen"
+          component={MainScreen}
+          options={{title: 'Main Screen'}}
+        />
+        <Stack.Screen name="AboutScreen" component={AboutScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
