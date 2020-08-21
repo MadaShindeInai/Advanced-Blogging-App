f3057b7d49715d7775304807fcba42f3db726c29
Sergei Nepryakhin <sergey.nepryahin@dashboquet.com>
2020-08-22 00:53:39 +0300

    feat: add drawer navigation. implement usability of the all screens

--- src/modules/App/index.tsx
+++ src/modules/App/index.tsx
@@ -3,7 +3,8 @@ import React, {FC} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {Button, Platform} from 'react-native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {Platform} from 'react-native';
 import {HeaderButtons, Item} from 'react-navigation-header-buttons';
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 import {THEME} from 'src/theme';
@@ -20,6 +21,8 @@ import {options as dateOptions} from '../../../constanst';
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 const AndroidTab = createMaterialBottomTabNavigator();
+const MainNavigator = createDrawerNavigator();
+
 const screenOpts: any = {
   headerStyle: {
     backgroundColor:
@@ -51,17 +54,34 @@ const postScreenOpts = ({route}: any) => ({
   ),
 });
 
+const aboutCreateOpts = ({navigation}: any) => ({
+  headerTitle: (props: any) => <LogoTitle {...props} />,
+  headerLeft: () => (
+    <HeaderButtons HeaderButtonComponent={HeaderIcon}>
+      <Item
+        title="Toggle Drawer"
+        iconName="navicon"
+        onPress={() => navigation.toggleDrawer()}
+      />
+    </HeaderButtons>
+  ),
+});
+
 function HomeScreenNavigator() {
   return (
     <Stack.Navigator screenOptions={screenOpts}>
       <Stack.Screen
         name="MainScreen"
         component={MainScreen}
-        options={{
+        options={({navigation}: any) => ({
           headerTitle: (props: any) => <LogoTitle {...props} />,
           headerRight: () => (
             <HeaderButtons HeaderButtonComponent={HeaderIcon}>
-              <Item title="Take photo" iconName="camera" onPress={() => {}} />
+              <Item
+                title="Take photo"
+                iconName="camera"
+                onPress={() => navigation.push('CreateScreen')}
+              />
             </HeaderButtons>
           ),
           headerLeft: () => (
@@ -69,28 +89,39 @@ function HomeScreenNavigator() {
               <Item
                 title="Toggle Drawer"
                 iconName="navicon"
-                onPress={() => {}}
+                onPress={() => navigation.toggleDrawer()}
               />
             </HeaderButtons>
           ),
-        }}
+        })}
       />
-      <Stack.Screen name="AboutScreen" component={AboutScreen} />
       <Stack.Screen
         name="PostScreen"
         component={PostScreen}
-        initialParams={{itemId: 1}}
         options={postScreenOpts}
       />
+      <Stack.Screen name="CreateScreen" component={CreateScreenNavigator} />
+    </Stack.Navigator>
+  );
+}
+function AboutScreenNavigator() {
+  return (
+    <Stack.Navigator screenOptions={screenOpts}>
+      <Stack.Screen
+        name="AboutScreen"
+        component={AboutScreen}
+        options={aboutCreateOpts}
+      />
+    </Stack.Navigator>
+  );
+}
+function CreateScreenNavigator() {
+  return (
+    <Stack.Navigator screenOptions={screenOpts}>
       <Stack.Screen
         name="CreateScreen"
         component={CreateScreen}
-        options={{
-          headerTitle: (props: any) => <LogoTitle {...props} />,
-          headerRight: () => (
-            <Button onPress={() => {}} title="Info" color="black" />
-          ),
-        }}
+        options={aboutCreateOpts}
       />
     </Stack.Navigator>
   );
@@ -102,18 +133,18 @@ function TabScreenNavigator() {
       <Stack.Screen
         name="BookedScreen"
         component={BookedScreen}
-        options={{
+        options={({navigation}) => ({
           headerTitle: (props: any) => <LogoTitle {...props} />,
           headerLeft: () => (
             <HeaderButtons HeaderButtonComponent={HeaderIcon}>
               <Item
                 title="Toggle Drawer"
                 iconName="navicon"
-                onPress={() => {}}
+                onPress={() => navigation.toggleDrawer()}
               />
             </HeaderButtons>
           ),
-        }}
+        })}
       />
       <Stack.Screen
         name="PostScreen"
@@ -124,58 +155,70 @@ function TabScreenNavigator() {
   );
 }
 
+function PostNavigator() {
+  if (Platform.OS === 'android') {
+    return (
+      <AndroidTab.Navigator
+        activeColor={THEME.colors.WHITE}
+        barStyle={{backgroundColor: THEME.colors.MAIN}}
+        shifting>
+        <AndroidTab.Screen
+          name="Home"
+          component={HomeScreenNavigator}
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({color}) => (
+              <FontAwesome name="home" color={color} size={24} />
+            ),
+          }}
+        />
+        <AndroidTab.Screen
+          name="Favorites"
+          component={TabScreenNavigator}
+          options={{
+            tabBarLabel: 'Favorites',
+            tabBarIcon: ({color}) => (
+              <FontAwesome name="star" color={color} size={24} />
+            ),
+          }}
+        />
+      </AndroidTab.Navigator>
+    );
+  }
+
+  return (
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({color, size}) => {
+          let iconName = 'ios-list-box';
+
+          if (route.name === 'Home') {
+            iconName = 'home';
+          } else if (route.name === 'Favorites') {
+            iconName = 'star';
+          }
+          // You can return any component that you like here!
+          return <FontAwesome name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: THEME.colors.MAIN,
+        inactiveTintColor: THEME.colors.SHADOW,
+      }}>
+      <Tab.Screen name="Home" component={HomeScreenNavigator} />
+      <Tab.Screen name="Favorites" component={TabScreenNavigator} />
+    </Tab.Navigator>
+  );
+}
+
 const App: FC = () => {
   return (
     <NavigationContainer>
-      {Platform.OS === 'android' ? (
-        <AndroidTab.Navigator
-          activeColor={THEME.colors.WHITE}
-          barStyle={{backgroundColor: THEME.colors.MAIN}}
-          shifting>
-          <AndroidTab.Screen
-            name="Home"
-            component={HomeScreenNavigator}
-            options={{
-              tabBarLabel: 'Home',
-              tabBarIcon: ({color}) => (
-                <FontAwesome name="home" color={color} size={24} />
-              ),
-            }}
-          />
-          <AndroidTab.Screen
-            name="Favorites"
-            component={TabScreenNavigator}
-            options={{
-              tabBarLabel: 'Favorites',
-              tabBarIcon: ({color}) => (
-                <FontAwesome name="star" color={color} size={24} />
-              ),
-            }}
-          />
-        </AndroidTab.Navigator>
-      ) : (
-        <Tab.Navigator
-          screenOptions={({route}) => ({
-            tabBarIcon: ({color, size}) => {
-              let iconName = 'ios-list-box';
-
-              if (route.name === 'Home') {
-                iconName = 'home';
-              } else if (route.name === 'Favorites') {
-                iconName = 'star';
-              }
-              // You can return any component that you like here!
-              return <FontAwesome name={iconName} size={size} color={color} />;
-            },
-          })}
-          tabBarOptions={{
-            activeTintColor: THEME.colors.MAIN,
-            inactiveTintColor: THEME.colors.SHADOW,
-          }}>
-          <Tab.Screen name="Home" component={HomeScreenNavigator} />
-          <Tab.Screen name="Favorites" component={TabScreenNavigator} />
-        </Tab.Navigator>
-      )}
+      <MainNavigator.Navigator>
+        <MainNavigator.Screen name="Home" component={PostNavigator} />
+        <MainNavigator.Screen name="About" component={AboutScreenNavigator} />
+        <MainNavigator.Screen name="Create" component={CreateScreenNavigator} />
+      </MainNavigator.Navigator>
     </NavigationContainer>
   );
 };
--- src/screens/AboutScreen/index.tsx
+++ src/screens/AboutScreen/index.tsx
@@ -1,27 +1,12 @@
 import React, {FC} from 'react';
-import {Text, View, Button, StatusBar} from 'react-native';
+import {View, Button, StatusBar} from 'react-native';
 import {styles} from './styles';
 
-export const AboutScreen: FC<any> = ({route, navigation}) => {
-  const {itemId, name, rate} = route.params;
+export const AboutScreen: FC<any> = ({navigation}) => {
   return (
     <>
       <StatusBar barStyle="dark-content" backgroundColor="#f4511e" />
       <View style={styles.screen}>
-        <Text style={styles.title}>AboutScreen</Text>
-        <Text style={styles.desc}>Person Id: {JSON.stringify(itemId)}</Text>
-        <Text style={styles.desc}>Name: {JSON.stringify(name)}</Text>
-        <Text style={styles.desc}>Rate: {JSON.stringify(rate)}</Text>
-        <Button
-          title="Go to About"
-          onPress={() =>
-            navigation.push('AboutScreen', {
-              itemId: 1,
-              name: 'Sasha',
-              rate: '3%',
-            })
-          }
-        />
         <Button
           title="Update the title"
           onPress={() => navigation.setOptions({title: 'Sasha yhodi!'})}
